3f81a010aba6b4bbd9e75f8ab3c1550fedb2a70a
madura <madura@sqrmobile.com>
2020-01-01 18:57:08 +0530

    added old src

--- src/index.js
+++ src/index.js
@@ -6,14 +6,12 @@ const loaderUtils = require('loader-utils');
 const MIMES = {
   'jpg': 'image/jpeg',
   'jpeg': 'image/jpeg',
-  'png': 'image/png',
-  'webp': 'image/webp',
+  'png': 'image/png'
 };
 
 const EXTS = {
   'image/jpeg': 'jpg',
-  'image/png': 'png',
-  'image/webp': 'webp',
+  'image/png': 'png'
 };
 
 type Config = {
@@ -23,16 +21,47 @@ type Config = {
   max: string | number | void,
   steps: string | number | void,
   name: string | void,
+  outputPath: Function | string | void,
+  publicPath: Function | string | void,
   context: string | void,
   placeholderSize: string | number | void,
   quality: string | number | void,
   background: string | number | void,
   placeholder: string | boolean | void,
   adapter: ?Function,
-  // for backward compatibility
   format: 'png' | 'jpg' | 'jpeg',
-  formats: ['png' | 'jpg' | 'jpeg' | 'webp'],
   disable: ?boolean,
+};
+
+const getOutputAndPublicPath = (fileName:string, {outputPath: configOutputPath, publicPath: configPublicPath}:Config) => {
+  let outputPath = fileName;
+
+  if (configOutputPath) {
+    if (typeof configOutputPath === 'function') {
+      outputPath = configOutputPath(fileName);
+    } else {
+      outputPath = path.posix.join(configOutputPath, fileName);
+    }
+  }
+
+  let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`;
+
+  if (configPublicPath) {
+    if (typeof configPublicPath === 'function') {
+      publicPath = configPublicPath(fileName);
+    } else if (configPublicPath.endsWith('/')) {
+      publicPath = configPublicPath + fileName;
+    } else {
+      publicPath = `${configPublicPath}/${fileName}`;
+    }
+
+    publicPath = JSON.stringify(publicPath);
+  }
+
+  return {
+    outputPath,
+    publicPath
+  };
 };
 
 module.exports = function loader(content: Buffer) {
@@ -47,28 +76,23 @@ module.exports = function loader(content: Buffer) {
   // Useful when converting from PNG to JPG
   const background: string | number | void = config.background;
   // Specify mimetype to convert to another format
-  let originalExtension = path.extname(this.resourcePath).replace(/\./, '');
-  let formats: [string];
+  let mime: string;
+  let ext: string;
   if (config.format) {
-    formats = [config.format];
-  } else if (config.formats) {
-    formats = config.formats;
+    if (!MIMES.hasOwnProperty(config.format)) {
+      return loaderCallback(new Error('Format "' + config.format + '" not supported'));
+    }
+    mime = MIMES[config.format];
+    ext = EXTS[mime];
   } else {
-    formats = originalExtension;
+    ext = path.extname(this.resourcePath).replace(/\./, '');
+    mime = MIMES[ext];
+    if (!mime) {
+      return loaderCallback(new Error('No mime type for file with extension ' + ext + 'supported'));
+    }
   }
 
-  // throw a more friendly error than jimp failing to encode
-  if (!config.adapter && formats.find(f => f === 'webp')) {
-    return loaderCallback(new Error('JIMP does not support webp encoding, use sharp adapter.'));
-  }
-
-  const mimes = formats.map(f => MIMES[f]);
-  const errFormats = mimes.reduce((m, i) => !m ? [...acc, formats[i]] : acc, []);
-  if (errFormats.length > 0) {
-    return loaderCallback(new Error('Formats not supported: ', JSON.stringify(errFormats)));
-  }
-
-  const name = (config.name || '[hash]-[width].[ext]');
+  const name = (config.name || '[hash]-[width].[ext]').replace(/\[ext\]/ig, ext);
 
   const adapter: Function = config.adapter || require('./adapters/jimp');
   const loaderContext: any = this;
@@ -101,40 +125,41 @@ module.exports = function loader(content: Buffer) {
 
   if (config.disable) {
     // emit original content only
-    const f = loaderUtils.interpolateName(loaderContext, name, {
+    const fileName = loaderUtils.interpolateName(loaderContext, name, {
       context: outputContext,
       content: content
     })
       .replace(/\[width\]/ig, '100')
-      .replace(/\[height\]/ig, '100')
-      .replace(/\[ext\]/ig, originalExtension);
-    loaderContext.emitFile(f, content);
-    const p = '__webpack_public_path__ + ' + JSON.stringify(f);
-    return loaderCallback(null, 'module.exports = {srcSet:' + p + ',images:[{path:' + p + ',width:100,height:100}],src: ' + p + ',toString:function(){return ' + p + '}};');
+      .replace(/\[height\]/ig, '100');
+
+    const {outputPath, publicPath} = getOutputAndPublicPath(fileName, config);
+
+    loaderContext.emitFile(outputPath, content);
+
+    return loaderCallback(null, 'module.exports = {srcSet:' + publicPath + ',images:[{path:' + publicPath + ',width:100,height:100}],src: ' + publicPath + ',toString:function(){return ' + publicPath + '}};');
   }
 
-  const createFile = (mime, {data, width, height}) => {
-    const ext = EXTS[mime];
-
+  const createFile = ({data, width, height}) => {
     const fileName = loaderUtils.interpolateName(loaderContext, name, {
       context: outputContext,
       content: data
     })
-      .replace(/\[width\]/ig, width)
-      .replace(/\[height\]/ig, height)
-      .replace(/\[ext\]/ig, ext);
+    .replace(/\[width\]/ig, width)
+    .replace(/\[height\]/ig, height);
 
-    loaderContext.emitFile(fileName, data);
+    const {outputPath, publicPath} = getOutputAndPublicPath(fileName, config);
+
+    loaderContext.emitFile(outputPath, data);
 
     return {
-      src: '__webpack_public_path__ + ' + JSON.stringify(fileName + ' ' + width + 'w'),
-      path: '__webpack_public_path__ + ' + JSON.stringify(fileName),
+      src: publicPath + `+${JSON.stringify(` ${width}w`)}`,
+      path: publicPath,
       width: width,
       height: height
     };
   };
 
-  const createPlaceholder = (mime, {data}: {data: Buffer}) => {
+  const createPlaceholder = ({data}: {data: Buffer}) => {
     const placeholder = data.toString('base64');
     return JSON.stringify('data:' + (mime ? mime + ';' : '') + 'base64,' + placeholder);
   };
@@ -142,7 +167,7 @@ module.exports = function loader(content: Buffer) {
   const img = adapter(loaderContext.resourcePath);
   return img.metadata()
     .then((metadata) => {
-      const promises = mimes.map(() => []);
+      let promises = [];
       const widthsToGenerate = new Set();
 
       (Array.isArray(sizes) ? sizes : [sizes]).forEach((size) => {
@@ -151,68 +176,49 @@ module.exports = function loader(content: Buffer) {
         // Only resize images if they aren't an exact copy of one already being resized...
         if (!widthsToGenerate.has(width)) {
           widthsToGenerate.add(width);
-
-          mimes.forEach((mime, i) => {
-            promises[i] = img.resize({
-              width,
-              mime,
-              options: adapterOptions
-            });
-          });
+          promises.push(img.resize({
+            width,
+            mime,
+            options: adapterOptions
+          }));
         }
       });
 
-      return Promise.all(promises.map(arr => Promise.all(arr)))
-        .then(imagesArr => imagesArr
-          .map((images, i) => ({
-            files: images.map(img => createFile(mime, img)),
-            mime: mimes[i]
-          }))
-        )
-        .then(filesByMime => {
-          if (outputPlaceholder) {
-            return img.resize({
-              placeholderSize,
-              mime: mimes[0],
-              options: adapterOptions
-            })
-              .then(img => ({
-                filesByMime,
-                placeholder: createPlaceholder(mimes[0], img),
-              }));
+      if (outputPlaceholder) {
+        promises.push(img.resize({
+          width: placeholderSize,
+          options: adapterOptions,
+          mime
+        }));
+      }
+
+      return Promise.all(promises)
+        .then(results => outputPlaceholder
+          ? {
+            files: results.slice(0, -1).map(createFile),
+            placeholder: createPlaceholder(results[results.length - 1])
           }
-
-          return { filesByMime };
-        });
+          : {
+            files: results.map(createFile)
+          }
+         );
     })
-    .then(({filesByMime, placeholder}) => {
-      const srcSets = filesByMime.map(({ mime, files }) => ({
-        mime,
-        srcSet: files.map(f => f.src).join(' ')
-      }));
+    .then(({files, placeholder}) => {
+      const srcset = files.map(f => f.src).join('+","+');
 
-      const images = filesByMime.reduce((acc, {mime, files}) =>
-        acc.concat(
-          files.map(({mime, path, width, height}) => ({ path, width, height }))
-        )
-      , []);
+      const images = files.map(f => '{path:' + f.path + ',width:' + f.width + ',height:' + f.height + '}').join(',');
 
-      const firstImage = images[0];
+      const firstImage = files[0];
 
-      loaderCallback(null,`
-        const srcSets = ${JSON.stringify(srcSets)};
-
-        module.exports = {
-          srcSets,
-          srcSet: srcSets[0].srcSet,
-          images: ${JSON.stringify(images)};
-          src: ${firstImage.path},
-          toString:function(){return ${firstImage.path} },
-          placeholder: ${placeholder},
-          width: ${firstImage.width},
-          height: ${firstImage.height}
-        };
-      `);
+      loaderCallback(null, 'module.exports = {' +
+          'srcSet:' + srcset + ',' +
+          'images:[' + images + '],' +
+          'src:' + firstImage.path + ',' +
+          'toString:function(){return ' + firstImage.path + '},' +
+          'placeholder: ' + placeholder + ',' +
+          'width:' + firstImage.width + ',' +
+          'height:' + firstImage.height +
+      '};');
     })
     .catch(err => loaderCallback(err));
 };
